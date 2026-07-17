10e9995f9402a5d63e856ad279d9f45af8c418de
04076228456 <fernandofanf@fab.mil.br>
2026-07-17 11:24:19 -0300

    feat(frontend): cria shell da aplicação e integra Angular Material

--- frontend/src/app/layout/header/header.ts
+++ frontend/src/app/layout/header/header.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
