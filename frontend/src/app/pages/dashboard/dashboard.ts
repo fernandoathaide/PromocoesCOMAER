0aae92152c1347b767c47c085a8dd1aa4a697adc
04076228456 <fernandofanf@fab.mil.br>
2026-07-20 14:11:49 -0300

    feat(dashboard): exibe indicadores utilizando componente StatCard

--- frontend/src/app/pages/dashboard/dashboard.ts
+++ frontend/src/app/pages/dashboard/dashboard.ts
@@ -3,9 +3,14 @@ import { Component, OnInit, inject } from '@angular/core';
 import { Simulacao } from '../../core/models/simulacao.model';
 import { SimulacaoService } from '../../core/services/simulacao.service';
 
+import { StatCard } from '../../shared/components/stat-card/stat-card';
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
+  imports: [
+    StatCard
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
@@ -22,15 +27,11 @@ export class Dashboard implements OnInit {
       .subscribe({
 
         next: dados => {
-
           this.simulacao = dados;
-
         },
 
         error: erro => {
-
           console.error('Erro ao consultar API', erro);
-
         }
 
       });
