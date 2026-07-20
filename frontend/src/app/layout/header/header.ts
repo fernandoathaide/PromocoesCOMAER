b8164bbde7063f94a75722b99d9ac0c17dc2ce98
04076228456 <fernandofanf@fab.mil.br>
2026-07-20 10:45:43 -0300

    feat(layout): migra header para Angular Material Toolbar

--- frontend/src/app/layout/header/header.ts
+++ frontend/src/app/layout/header/header.ts
@@ -1,19 +1,17 @@
 import { Component } from '@angular/core';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [
     MatToolbarModule,
-    MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule
   ],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
-export class Header {
-
-}
+export class Header {}
