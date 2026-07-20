732a8abdd801f242997916e90884d0be92979926
04076228456 <fernandofanf@fab.mil.br>
2026-07-20 14:37:31 -0300

    feat(militares): implementa tabela de militares com Angular Material

--- frontend/src/app/pages/militares/militares.ts
+++ frontend/src/app/pages/militares/militares.ts
@@ -1,9 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, inject } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+
+import { Militar } from '../../core/models/militar.model';
+import { MilitarService } from '../../core/services/militar.service';
 
 @Component({
   selector: 'app-militares',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
+  ],
   templateUrl: './militares.html',
-  styleUrl: './militares.scss',
+  styleUrl: './militares.scss'
 })
-export class Militares {}
+export class Militares implements OnInit, AfterViewInit {
+
+  private readonly militarService = inject(MilitarService);
+
+  displayedColumns: string[] = [
+    'numero_ordem',
+    'nome',
+    'posto',
+    'quadro'
+  ];
+
+  dataSource = new MatTableDataSource<Militar>();
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
+  @ViewChild(MatSort)
+  sort!: MatSort;
+
+  ngOnInit(): void {
+
+    this.militarService.listar().subscribe({
+
+      next: militares => {
+        this.dataSource.data = militares;
+      },
+
+      error: erro => {
+        console.error(erro);
+      }
+
+    });
+
+  }
+
+  ngAfterViewInit(): void {
+
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+
+  }
+
+}
